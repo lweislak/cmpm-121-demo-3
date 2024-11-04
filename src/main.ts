4123dc895cf3a196d49e5dd12f1e715e62ea606f
lweislak <150990328+lweislak@users.noreply.github.com>
2024-11-03 19:53:01 -0800

    Test commit

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
-// todo
+import "./style.css";
+
+const APP_NAME = "Geocoin Carrier";
+const app = document.querySelector<HTMLDivElement>("#app")!;
+document.title = APP_NAME;
+
+const button = document.createElement("button");
+app.append(button);
+
+button.addEventListener("click", function () {
+  alert("You clicked the button!");
+});
