913e1460930b7e78228f63a1e60ffa2d588d9cc9
lweislak <150990328+lweislak@users.noreply.github.com>
2024-11-18 15:10:15 -0800

    Deleted examples and added board

---
+++ src/board.ts
@@ -0,0 +1,53 @@
+import leaflet from "leaflet";
+
+interface Cell {
+  readonly i: number;
+  readonly j: number;
+}
+
+export class Board {
+  readonly tileWidth: number;
+  readonly tileVisibilityRadius: number;
+
+  private readonly knownCells: Map<string, Cell>;
+
+  constructor(tileWidth: number, tileVisibilityRadius: number) {
+    this.tileWidth = tileWidth;
+    this.tileVisibilityRadius = tileVisibilityRadius;
+    this.knownCells = new Map();
+  }
+
+  //Return the location of the cell at the index
+  private getCanonicalCell(cell: Cell): Cell {
+    const { i, j } = cell;
+    const key = [i, j].toString();
+    // ...
+    return this.knownCells.get(key)!;
+  }
+
+  //Return the index of the cell that the point is located in
+  getCellForPoint(point: leaflet.LatLng): Cell {
+    return this.getCanonicalCell({
+      i: Math.floor(point.lat / this.tileWidth),
+      j: Math.floor(point.lng / this.tileWidth),
+    });
+  }
+
+  //Returns bounds of cell
+  getCellBounds(cell: Cell): leaflet.LatLngBounds {
+    return leaflet.latLngBounds([
+      [cell.i * this.tileWidth, cell.j * this.tileWidth],
+      [(cell.i + 1) * this.tileWidth, (cell.j + 1) * this.tileWidth],
+    ]);
+  }
+
+  //Return cells located within radius of the given point
+  /*
+  getCellsNearPoint(point: leaflet.LatLng): Cell[] {
+      const resultCells: Cell[] = [];
+      const originCell = this.getCellForPoint(point);
+      // ...
+      return resultCells;
+  }
+      */
+}
--- src/main.ts
+++ src/main.ts
@@ -7,26 +7,20 @@ document.title = APP_NAME;
 // @deno-types="npm:@types/leaflet@^1.9.14"
 import leaflet from "leaflet";
 
-// Style sheets
 import "leaflet/dist/leaflet.css";
 import "./style.css";
-
-// Fix missing marker images
 import "./leafletWorkaround.ts";
-
-// Deterministic random number generator
 import luck from "./luck.ts";
 
-// Location of our classroom (as identified on Google Maps)
-const OAKES_CLASSROOM = leaflet.latLng(36.98949379578401, -122.06277128548504);
+//import { Board } from "./board.ts";
 
-// Tunable gameplay parameters
+const OAKES_CLASSROOM = leaflet.latLng(36.98949379578401, -122.06277128548504);
 const GAMEPLAY_ZOOM_LEVEL = 19;
 const TILE_DEGREES = 1e-4;
 const NEIGHBORHOOD_SIZE = 8;
 const CACHE_SPAWN_PROBABILITY = 0.1;
+//const board = new Board(TILE_DEGREES, NEIGHBORHOOD_SIZE);
 
-// Create the map (element with id "map" is defined in index.html)
 const map = leaflet.map(document.getElementById("map")!, {
   center: OAKES_CLASSROOM,
   zoom: GAMEPLAY_ZOOM_LEVEL,
@@ -52,13 +46,14 @@ playerMarker.addTo(map);
 
 // Display the player's points
 let playerPoints = 0;
-const statusPanel = document.querySelector<HTMLDivElement>("#statusPanel")!; // element `statusPanel` is defined in index.html
+const statusPanel = document.querySelector<HTMLDivElement>("#statusPanel")!;
 statusPanel.innerHTML = `Points: ${playerPoints}`;
 
 // Add caches to the map by cell numbers
 function spawnCache(i: number, j: number) {
   // Convert cell numbers into lat/lng bounds
   const origin = OAKES_CLASSROOM;
+  //NOTE: Moved bounds code to getCellBounds in board.ts
   const bounds = leaflet.latLngBounds([
     [origin.lat + i * TILE_DEGREES, origin.lng + j * TILE_DEGREES],
     [origin.lat + (i + 1) * TILE_DEGREES, origin.lng + (j + 1) * TILE_DEGREES],
@@ -70,14 +65,11 @@ function spawnCache(i: number, j: number) {
 
   // Handle interactions with the cache
   rect.bindPopup(() => {
-    // Each cache has a random point value, mutable by the player
-    let pointValue = Math.floor(luck([i, j, "initialValue"].toString()) * 100);
+    let pointValue = Math.floor(luck([i, j, "initialValue"].toString()) * 5);
 
-    // The popup offers a description and button
-    //Note: Fix deposit button/ querySelector
     const popupDiv = document.createElement("div");
     popupDiv.innerHTML = `
-                <div>There is a cache here at "${i},${j}". It has <span id="value">${pointValue}</span></div> points
+                <div>Location: ("${i},${j}"). Points avaliable: <span id="value">${pointValue}</span></div>
                 <button id="collect">Collect</button>
                 <button id="deposit">Deposit</button>`;
 
